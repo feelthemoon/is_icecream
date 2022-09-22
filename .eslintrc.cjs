c61fe5449496031e5c4d3626fda4fb5bdd5bcc46
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-09-22 19:30:17 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  plugins: ["prettier", "import"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "vue",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["vue"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src/"]],
+        extensions: [".ts", ".vue", ".js", ".spec.ts"],
+      },
+    },
+  },
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="logo-box">
+    <img class="logo vite" src="@/assets/vite.svg" />
+    <img class="logo electron" src="@/assets/electron.svg" />
+    <img class="logo vue" src="@/assets/vue.svg" />
+    <el-button type="primary">Default</el-button>
+  </div>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="static-public">
+    Place static files into the <code>/public</code> folder
+    <img style="width: 77px" :src="'./node.png'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
