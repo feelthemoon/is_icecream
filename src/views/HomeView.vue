f569ae03d5e62698dba5d09f27f455440c846d50
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-09-23 12:15:01 +0300

    feat(hot_fixes): hot fixes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,5 +15,6 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ElButton } from "element-plus";
 import HelloWorld from "@/components/HelloWorld.vue";
 </script>
