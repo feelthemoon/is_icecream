c7b0951fd40eb7afb7ee721c0ada0bca61eecd34
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-09-29 21:35:50 +0300

    chore: added transition for routing

--- src/layouts/Default/Default.vue
+++ src/layouts/Default/Default.vue
@@ -2,6 +2,10 @@
   <main
     class="layout layout--default my-0 mx-auto max-w-1200px w-full h-full flex justify-center items-center"
   >
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <Transition name="slide-fade" mode="out-in">
+        <component :is="Component"></component>
+      </Transition>
+    </router-view>
   </main>
 </template>
