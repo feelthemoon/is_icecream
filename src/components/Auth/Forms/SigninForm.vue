a2b80a7e561d97042b4f891dfe64068a4987b061
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-10-03 23:56:52 +0300

    feat(big updates): connect api && processing form errors && rework and big update for form validation

--- src/components/Auth/Forms/SigninForm.vue
+++ src/components/Auth/Forms/SigninForm.vue
@@ -1,22 +1,27 @@
 <template>
   <el-form
-    ref="ruleFormRef"
     status-icon
     class="flex flex-col items-center"
-    :model="ruleForm"
-    :rules="rules"
-    @submit.prevent="submitForm(ruleFormRef)"
+    :model="formData"
+    @submit.prevent="submitForm"
+    novalidate
   >
-    <el-form-item class="min-w-420px !mb-5" prop="email">
+    <el-form-item
+      class="min-w-420px !mb-5"
+      :error="invalidEmail || apiErrors[0]?.message"
+    >
       <el-input
-        v-model.trim="ruleForm.email"
+        v-model.trim="formData.email"
         type="email"
         :placeholder="t('placeholders.signin.email')"
       />
     </el-form-item>
-    <el-form-item class="min-w-420px !mb-2" prop="password">
+    <el-form-item
+      class="min-w-420px !mb-2"
+      :error="invalidPassword || apiErrors[0]?.message"
+    >
       <el-input
-        v-model.trim="ruleForm.password"
+        v-model.trim="formData.password"
         type="password"
         :placeholder="t('placeholders.signin.password')"
       />
@@ -29,7 +34,7 @@
       >
     </el-form-item>
     <el-form-item class="!mb-0">
-      <el-button type="primary" native-type="submit">{{
+      <el-button :loading="loading" type="primary" native-type="submit">{{
         t("pages.signin.form.buttonText")
       }}</el-button>
     </el-form-item>
@@ -37,45 +42,52 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { computed, reactive } from "vue";
 
 import { ElInput, ElButton, ElForm, ElFormItem } from "element-plus";
-import type { FormInstance, FormRules } from "element-plus";
 import { useI18n } from "vue-i18n";
 
-import { log } from "@/utils";
+import { useAuth, useRootStore } from "@/stores";
+import { useForm } from "@/utils/hooks";
 
 const { t } = useI18n();
+const { signin } = useAuth();
+const rootStore = useRootStore();
 
-const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive({
+const formData = reactive({
   email: "",
   password: "",
 });
 
-const rules = reactive<FormRules>({
-  email: [
-    {
-      required: true,
-      message: t("validation.invalid_email"),
-    },
-  ],
-  password: [
-    {
-      required: true,
-      message: t("validation.required_field"),
-    },
-  ],
-});
+const { v$, createValidationMessage } = useForm(formData, [
+  {
+    field: "email",
+    validators: ["required", "emailValidator"],
+    validationMessage: t("validation.invalid_email"),
+  },
+  {
+    field: "password",
+    validators: ["required"],
+    validationMessage: t("validation.required_field"),
+  },
+]);
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      log("submit!");
-    } else {
-      log("error submit!", fields);
-    }
-  });
+const invalidEmail = createValidationMessage("email");
+const invalidPassword = createValidationMessage("password");
+
+const loading = computed(() =>
+  rootStore.loading.some(
+    (loadingModule) => loadingModule.currentLoadingName === "signin"
+  )
+);
+const apiErrors = computed(() =>
+  rootStore.errors.filter((error) => error.namespace === "signin")
+);
+
+const submitForm = async () => {
+  await v$.value.$validate();
+  if (!v$.value.$invalid) {
+    await signin(formData);
+  }
 };
 </script>
--- src/components/Auth/Forms/SignupForm.vue
+++ src/components/Auth/Forms/SignupForm.vue
@@ -1,36 +1,46 @@
 <template>
   <el-form
-    ref="ruleFormRef"
     status-icon
     class="flex flex-col items-center"
-    :model="ruleForm"
-    :rules="rules"
-    @submit.prevent="submitForm(ruleFormRef)"
+    :model="formData"
+    @submit.prevent="submitForm"
   >
-    <el-form-item class="min-w-420px !mb-5" prop="name">
+    <el-form-item
+      class="min-w-420px !mb-5"
+      prop="name"
+      :error="invalidFirstName"
+    >
       <el-input
         maxlength="25"
-        v-model.trim="ruleForm.name"
+        v-model.trim="formData.first_name"
         :placeholder="t('placeholders.signup.name')"
       />
     </el-form-item>
-    <el-form-item class="min-w-420px !mb-5" prop="email">
+    <el-form-item
+      class="min-w-420px !mb-5"
+      prop="email"
+      :error="invalidEmail || emailApiError?.message"
+    >
       <el-input
-        v-model.trim="ruleForm.email"
+        v-model.trim="formData.email"
         type="email"
         :placeholder="t('placeholders.signup.email')"
       />
     </el-form-item>
-    <el-form-item class="min-w-420px !mb-6" prop="password">
+    <el-form-item
+      class="min-w-420px !mb-6"
+      prop="password"
+      :error="invalidPassword"
+    >
       <el-input
-        v-model.trim="ruleForm.password"
+        v-model.trim="formData.password"
         maxlength="255"
         type="password"
         :placeholder="t('placeholders.signup.password')"
       />
     </el-form-item>
     <el-form-item class="!mb-0">
-      <el-button type="primary" native-type="submit">{{
+      <el-button :loading="loading" type="primary" native-type="submit">{{
         t("pages.signup.form.buttonText")
       }}</el-button>
     </el-form-item>
@@ -38,58 +48,67 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { computed, reactive } from "vue";
 
 import { ElInput, ElButton, ElForm, ElFormItem } from "element-plus";
-import type { FormInstance, FormRules } from "element-plus";
 import { useI18n } from "vue-i18n";
 
-import { useCustomValidators, log } from "@/utils";
+import { useAuth, useRootStore } from "@/stores";
+import { useForm } from "@/utils/hooks";
 
 const { t } = useI18n();
-const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive({
+const formData = reactive({
   email: "",
   password: "",
-  name: "",
+  first_name: "",
 });
 
-const { emailValidator } = useCustomValidators();
+const { signup } = useAuth();
 
-const rules = reactive<FormRules>({
-  email: [
-    {
-      validator: emailValidator,
-      message: t("validation.invalid_email"),
-      trigger: "blur",
-    },
-  ],
-  password: [
-    {
-      required: true,
-      message: t("validation.min_length", { field: "пароля", length: 8 }),
-      min: 8,
-      trigger: "blur",
-    },
-  ],
-  name: [
-    {
-      required: true,
-      message: t("validation.min_length", { field: "имени", length: 3 }),
-      min: 3,
-      trigger: "blur",
-    },
-  ],
-});
+const rootStore = useRootStore();
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      log("submit!");
-    } else {
-      log("error submit!", fields);
-    }
-  });
+const { v$, createValidationMessage } = useForm(formData, [
+  {
+    field: "email",
+    validators: ["required", "emailValidator"],
+    validationMessage: t("validation.invalid_email"),
+  },
+  {
+    field: "password",
+    validators: [{ type: "minLength", validatorValue: 8 }, "required"],
+    validationMessage: t("validation.min_length", {
+      field: "пароля",
+      length: 8,
+    }),
+  },
+  {
+    field: "first_name",
+    validators: [{ type: "minLength", validatorValue: 3 }, "required"],
+    validationMessage: t("validation.min_length", {
+      field: "имени",
+      length: 3,
+    }),
+  },
+]);
+
+const loading = computed(() =>
+  rootStore.loading.some(
+    (loadingModule) => loadingModule.currentLoadingName === "signup"
+  )
+);
+
+const invalidEmail = createValidationMessage("email");
+const invalidPassword = createValidationMessage("password");
+const invalidFirstName = createValidationMessage("first_name");
+
+const emailApiError = computed(() =>
+  rootStore.errorByType("invalid_data_email", "signup")
+);
+
+const submitForm = async () => {
+  await v$.value.$validate();
+  if (!v$.value.$invalid) {
+    await signup(formData);
+  }
 };
 </script>
