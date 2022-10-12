b253a7bc9a9fc269075cf84cd09cf98b28169c4f
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-10-12 23:09:07 +0300

    refactor: added 'needsAuth' option for proposing authorization header

--- src/api/modules/users.api.ts
+++ src/api/modules/users.api.ts
@@ -4,13 +4,11 @@ import routes from "@/config/api/routes.api";
 
 import createRequest from "../_base.api";
 
-export const createUserGetInfoRequest = (
-  token: string
-): Promise<AxiosResponse | void> =>
-  createRequest({
-    method: "GET",
-    route: routes.users("me"),
-    headers: {
-      authorization: token,
+export const createUserGetInfoRequest = (): Promise<AxiosResponse | void> =>
+  createRequest(
+    {
+      method: "GET",
+      route: routes.users("me"),
     },
-  });
+    { needsAuth: true }
+  );
--- src/stores/users.store.ts
+++ src/stores/users.store.ts
@@ -5,14 +5,11 @@ import { defineStore } from "pinia";
 import { createUserGetInfoRequest } from "@/api";
 import { User } from "@/config/app/types";
 
-import { useRootStore } from "./root.store";
-
 export const useUsersStore = defineStore("users", () => {
-  const rootStore = useRootStore();
   const me: Ref<User | null> = ref(null);
 
   const getMe = async () => {
-    const response = await createUserGetInfoRequest(rootStore.token);
+    const response = await createUserGetInfoRequest();
     if (response?.data) {
       me.value = response.data;
     }
